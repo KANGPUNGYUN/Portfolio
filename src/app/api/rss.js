ef93d6c8957789ba6429be376a86c5ded9f6c795
PUNGY <71264780+KANGPUNGYUN@users.noreply.github.com>
2024-04-07 18:49:18 +0900

    test: get api

--- src/app/api/rss.js
+++ src/app/api/rss.js
@@ -5,25 +5,25 @@ export default async function handler(req, res) {
   try {
     const response = await axios.get("https://v2.velog.io/rss/@kangpungyun");
     const xmlData = response.data;
-    // const xmlDocs = new DOMParser().parseFromString(xmlData, "text/xml");
-    // const items = xmlDocs.getElementsByTagName("item");
+    const xmlDocs = new DOMParser().parseFromString(xmlData, "text/xml");
+    const items = xmlDocs.getElementsByTagName("item");
 
     console.log(xmlData);
-    // const postsData = [];
-    // for (let key in items) {
-    //   if (items.hasOwnProperty(key)) {
-    //     let item = items[key];
-    //     if (typeof item === "object" && item !== null) {
-    //       postsData.push({
-    //         title: item.getElementsByTagName("title")[0].textContent,
-    //         url: item.getElementsByTagName("link")[0].textContent,
-    //         createdAt: item.getElementsByTagName("pubDate")[0].textContent,
-    //       });
-    //     }
-    //   }
-    // }
-    // postsData.pop();
-    // res.status(200).json(postsData);
+    const postsData = [];
+    for (let key in items) {
+      if (items.hasOwnProperty(key)) {
+        let item = items[key];
+        if (typeof item === "object" && item !== null) {
+          postsData.push({
+            title: item.getElementsByTagName("title")[0].textContent,
+            url: item.getElementsByTagName("link")[0].textContent,
+            createdAt: item.getElementsByTagName("pubDate")[0].textContent,
+          });
+        }
+      }
+    }
+    postsData.pop();
+    res.status(200).json(postsData);
   } catch (error) {
     console.error("Error fetching RSS:", error);
     res.status(500).json({ error: "Error fetching RSS" });
